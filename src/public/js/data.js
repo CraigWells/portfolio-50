84e8a5cc16d0270aba7cb835f749e149b67d56dd
Craig Wells <craigdavidwells@aol.com>
2016-03-28 23:42:21 +0100

    Small graphs integration

---
+++ src/public/js/data.js
@@ -0,0 +1,86 @@
+﻿var data = [
+	{
+		name : "2017 Sales forecast",
+		description : "Based upon current trends",
+		details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+		columnValues : {
+			"value_1" : 200,
+			"value_2" : 206,
+			"value_3" : 207,
+			"value_4" : 300,
+			"value_5" : 303,
+			"value_6" : 280,
+			"value_7" : 278,
+			"value_8" : 290
+		}
+	},
+	{
+		name : "Growth 4th Quarter 2016",
+		description : "Actual growth",
+		details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+		columnValues : {
+			"value_1" : 186,
+			"value_2" : 199,
+			"value_3" : 201,
+			"value_4" : 222,
+			"value_5" : 250,
+			"value_6" : 234,
+			"value_7" : 236,
+			"value_8" : 240,
+			"value_9" : 210,
+			"value_10" : 220,
+			"value_11" : 230,
+			"value_12" : 244
+		}
+	},
+	{
+		name : "Training expences 2012 - 2015",
+		description : "Investment in personnel development",
+		details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+		columnValues : {
+			"value_1" : 1,
+			"value_2" : 2,
+			"value_3" : 3,
+			"value_4" : 4,
+			"value_5" : 5,
+			"value_6" : 6,
+			"value_7" : 7,
+			"value_8" : 8,
+			"value_9" : 9,
+			"value_10" : 10,
+			"value_11" : 11,
+			"value_12" : 12
+		}
+	},
+	{
+		name : "Gross expenditure 2016",
+		description : "Total annual costs, year to date",
+		details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+		columnValues : {
+			"value_1" : 900,
+			"value_2" : 830,
+			"value_3" : 730,
+			"value_4" : 832,
+			"value_5" : 789,
+			"value_6" : 578,
+			"value_7" : 456,
+			"value_8" : 345,
+			"value_9" : 678,
+			"value_10" : 610,
+			"value_11" : 419,
+			"value_12" : 712,
+			"value_13" : 900,
+			"value_14" : 830,
+			"value_15" : 300,
+			"value_16" : 400,
+			"value_17" : 500,
+			"value_18" : 600,
+			"value_19" : 700,
+			"value_20" : 800,
+			"value_21" : 900,
+			"value_22" : 810,
+			"value_23" : 711,
+			"value_24" : 612
+		}	
+	}		
+];
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -7,7 +7,13 @@ app.service('projectsService', function(){
           {
               name : "Graph Data",
               id : "graph-data",
-              img: "sm_graph.jpg"
+              img: "sm_graph.jpg",
+              config: {
+                name : "graph_data",
+                directive : {
+                    graph : true
+                }
+              }
           },
           {
               name : "Real Time Graphs",
@@ -57,6 +63,115 @@ app.service('projectsService', function(){
       ];
  });
 
+app.factory('graphData', function(){
+        // Perform the data manipulation 
+        var height = 80,
+            width = 170;
+
+        function init(){
+            var len = data.length;    
+            for(var i = 0; i < len; i++){
+                calculateValues(data[i], calculateDifs);
+                data[i].height = height;
+                data[i].width = width;
+            } 
+            return data;
+        };       
+
+        function calculateValues(item, callback){
+            var values = [];
+            for(var key in item.columnValues){
+                values.push(item.columnValues[key]);
+            }
+            item.values = values;
+            callback(item, plotPoints);
+        };
+
+        function calculateDifs(item, callback){
+            item.pointsY = [];
+            var len = item.values.length,
+                lowest = getLowestValue(item.values),
+                range = getHeighestValue(item.values) - lowest,
+                increment = height / range; 
+            for(var i=0; i<len;i++){
+                item.pointsY.push(height - (item.values[i] - lowest) * increment);
+            }
+            callback(item);
+        };
+
+        function plotPoints(item){
+            var range = item.pointsY.length - 1,
+                spacesX = Math.floor(width / range),
+                count = 0;
+            item.pointsX = [];
+            item.pointsX.push(count);
+            for(var i = 0; i < range; i++){
+                count += spacesX;
+                item.pointsX.push(count);
+            }
+        };
+
+        function getHeighestValue(values){
+            return Math.max.apply(null, values);
+        };
+
+        function getLowestValue(values){
+            return Math.min.apply(null, values);
+        };
+
+        return init();
+    });
+
+
+ app.factory('graphRenderer', function(){
+
+        function drawGraph(item){
+
+            var settings = {
+                "a" : 0,
+                "b" : item.height/2,
+                "c" : item.width,
+                "d" : item.height/2,
+                "colour" : "#909090"
+            }; 
+
+            var drawingCanvas = document.getElementById(item.name);
+            var context = drawingCanvas.getContext("2d");
+            // draw median
+            context.beginPath();
+            context.moveTo(settings.a, settings.b);
+            context.lineTo(settings.c, settings.d);
+            context.strokeStyle = settings.colour;
+            context.stroke();
+            // draw graph line
+            var len = item.pointsX.length;
+            context.beginPath();
+            for(var i = 0; i < len; i++){
+                context.lineTo(item.pointsX[i], item.pointsY[i]);
+            }
+            context.strokeStyle="red";
+            context.stroke();            
+        };
+
+        function plotLine(){
+            var len = canvasItem.data.pointsX.length;
+            canvasItem.beginPath();
+            for(var i = 0; i < len; i++){
+                canvasItem.lineTo(canvasItem.data.pointsX[i], canvasItem.data.pointsY[i]);
+            }
+            canvasItem.strokeStyle="red";
+            canvasItem.stroke();
+        };
+
+        return function(graphData){
+
+            angular.forEach(graphData, function(item) {
+                drawGraph(item);
+            });
+
+        };
+    });
+
 app.config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
       $routeProvider
@@ -96,9 +211,21 @@ app.config(['$routeProvider', '$locationProvider',
     this.name = "ProjectCtrl";
     this.id = $routeParams.id;
   }])
-  .controller('DemoCtrl', ['$routeParams', function($routeParams) {
+  .controller('DemoCtrl', ['$routeParams', '$filter', 'projectsService', function($routeParams, $filter, projectsService) {
     this.name = "DemoCtrl";
     this.id = $routeParams.id;
+    this.project = projectConfigLookup(projectsService, this.id);
+
+    function projectConfigLookup(projectsService, id) {
+       var found = $filter('filter')(projectsService, {id: id}, true), selected;
+       if (found.length) {
+           selected = found[0];
+       } else {
+           selected = 'Not found';
+       }
+       return selected;
+    }
+
   }])
   .controller('HomeCtrl', ['$routeParams', 'projectsService', function($routeParams, projectsService) {
     this.name = "HomeCtrl";
@@ -130,6 +257,22 @@ app.config(['$routeProvider', '$locationProvider',
               });
           });
       };
-  });
+  }).directive('demo', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'views/content.html'
+    }
+ }).directive('graphs', ['graphData', '$timeout', 'graphRenderer', function(graphData, $timeout, graphRenderer) {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/graph.html',
+            link: function(scope, element) {
+                scope.graphs = graphData;
+                $timeout(function () {
+                    graphRenderer(graphData); 
+                });
+            }
+        }
+}]);
 
 })(window.angular);
